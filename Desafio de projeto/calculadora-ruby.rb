059b1c8230119ac11fd96ba34dec886887b34ccc
Lucian Gomes <luciangomes7@gmail.com>
2024-05-11 23:05:39 -0300

    correção do coamndo system, adição do break, adição do else e restirada de resultado

--- Desafio de projeto/calculadora-ruby.rb
+++ Desafio de projeto/calculadora-ruby.rb
@@ -1,23 +1,21 @@
-resultado = ''
 loop do
-    puts resultado
-puts "Calculadora Básica Ruby"
-puts 'Selecione uma das operações abaixo: '
-puts '1 - SOMA'
-puts '2 - SUBTRAÇÃO'
-puts '3 - MULTIPLICAÇÃO'
-puts '4 - DIVISÃO'
-puts '0 - SAIR'
+    puts "Calculadora Básica Ruby"
+    puts 'Selecione uma das operações abaixo: '
+    puts '1 - SOMA'
+    puts '2 - SUBTRAÇÃO'
+    puts '3 - MULTIPLICAÇÃO'
+    puts '4 - DIVISÃO'
+    puts '0 - SAIR'
 
-operacao = gets.chomp.to_f
+operacao = gets.chomp.to_i
 
 case operacao
     when operacao = 1
         puts 'Digite um número: '
-        num1 = gets.chomp.to_f
+        num1 = gets.chomp.to_i
 
         puts 'Digite outro número: '
-        num2 = gets.chomp.to_f
+        num2 = gets.chomp.to_i
 
         soma = num1 + num2
 
@@ -25,39 +23,40 @@ case operacao
 
     when operacao = 2
         puts 'Digite um número: '
-        num1 = gets.chomp.to_f
+        num1 = gets.chomp.to_i
 
         puts 'Digite outro número: '
-        num2 = gets.chomp.to_f
+        num2 = gets.chomp.to_i
 
         subi = num1 - num2
         puts "O resultado da operação é: #{subi}"
 
     when operacao = 3
         puts 'Digite um número: '
-        num1 = gets.chomp.to_f
+        num1 = gets.chomp.to_i
 
         puts 'Digite outro número: '
-        num2 = gets.chomp.to_f
+        num2 = gets.chomp.to_i
 
         multi = num1 * num2
         puts "O resultado da operação é: #{multi}"
 
     when operacao = 4
         puts 'Digite um número: '
-        num1 = gets.chomp.to_f
+        num1 = gets.chomp.to_i
 
         puts 'Digite outro número: '
-        num2 = gets.chomp.to_f
+        num2 = gets.chomp.to_i
 
         divi =  num1 / num2
         puts "O resultado da operação é: #{divi}"
 
     when operacao = 0
-
+        break
+    else
         puts "OPÇÃO INVÁLIDA, DIGITE UMA OPÇÃO DA LISTA"
-    break
+    
 end
 #Limpa tela
-System "clear"
+system "clear"
 end
